614c05b149b317a5a3085ef2a4a2b4dfdc8dae0e
Antonio Pérez <64748202+AntPerDev@users.noreply.github.com>
2022-10-02 16:25:19 +0200

    fin seccion 9

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import isAuthenticatedGuard from './auth-guard';
 
 const routes = [
   {
@@ -47,6 +48,7 @@ const routes = [
   {
     path: '/dbz',
     name: 'dbz',
+    beforeEnter: [ isAuthenticatedGuard ],
     component: () =>
       import(
         /*webpackChunkName: "DragonBallLayout" */ '@/modules/dbz/layouts/DragonBallLayout'
